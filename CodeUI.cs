a5cd499af671685f0987c0b0fee7ec8a8f6f38a5
Ivenaccip <45603061+Ivenaccip@users.noreply.github.com>
2018-12-11 03:40:04 -0600

    CodeUILevel
    It's only to know which is the level that the player is.

---
+++ CodeUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CodeUI : MonoBehaviour {
+
+	public static int LevelQuantity = 1;
+	Text LevelText;
+
+	// Use this for initialization
+	void Start () {
+		LevelQuantity = 1;
+        LevelText = GameObject.Find("LevelQuantityText").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider LevelCheck)
+	{
+		if(LevelCheck.tag == "CheckPoint")
+		{
+			gameObject.SetActive(false);
+			LevelQuantity++;
+			LevelText.text = LevelQuantity.ToString();
+		}
+	}
+}
